5cfb21b525d40b9bb71806be28d67dcfc3e3bcb4
Truong <nguyendangtruong22042002@gmail.com>
2024-08-20 18:16:33 +0700

    backend and frontend web layout success

---
+++ web/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My</h1>
+    </div>
+  );
+};
+
+export default HomePage;
