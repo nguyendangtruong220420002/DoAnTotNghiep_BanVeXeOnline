e37706477e320c1e7e7b2fae436bcb64f9f69bd4
Truong <nguyendangtruong22042002@gmail.com>
2024-09-27 13:56:14 +0700

    Auth OPT SMS Web add frontend  modal login,sigi

--- backend/src/services/firebaseConfig.js
+++ backend/src/services/firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../../credentials/serviceAccountKey.json');
+// const serviceAccount = require('../../credentials/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
