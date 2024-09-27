37d657da63d02f8f1ba0929d8e36bf2c41bc8119
Truong <nguyendangtruong22042002@gmail.com>
2024-09-27 13:52:55 +0700

    Auth OPT SMS Web add frontend  modal login,sigin......

--- web/src/pages/Firebase/firebaseConfig.jsx
+++ web/src/pages/Firebase/firebaseConfig.jsx
@@ -7,13 +7,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDnzMB5GEZmPJOxTZkci_HyoB26Wzba3-E",
-  authDomain: "vexeonline-66348.firebaseapp.com",
-  projectId: "vexeonline-66348",
-  storageBucket: "vexeonline-66348.appspot.com",
-  messagingSenderId: "1031025673750",
-  appId: "1:1031025673750:web:3bf34f3fad26c83a941737",
-  measurementId: "G-504BCBV4NN"
+  
 };
 
 // Initialize Firebase
