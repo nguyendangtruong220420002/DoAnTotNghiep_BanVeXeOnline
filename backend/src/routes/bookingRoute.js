2b1c30f6d8842887b40cce54c17788c06fa4edb2
PSThai <93112500+PSThai@users.noreply.github.com>
2024-11-30 01:08:23 +0700

    add getbooking

--- backend/src/routes/bookingRoute.js
+++ backend/src/routes/bookingRoute.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createBooking } = require('../controllers/bookingController');
+const { createBooking, getBookingByUser } = require('../controllers/bookingController');
 
 router.post('/add', createBooking);
-
-
+router.get('/getBookingByUser', getBookingByUser);
 
 
 module.exports = router;
