c772f465a50e6c1ef4b266703da14c67509bbd3f
nguyendangtruong220420002 <nguyendangtruong22042002@gmail.com>
2024-12-04 00:13:28 +0700

    updates run build

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '127.0.0.1',  // Hoặc '127.0.0.1' nếu bạn muốn dùng địa chỉ IP
-    port: 5173,            // Cổng bạn đang sử dụng, có thể thay đổi nếu cần
-    open: true,            // Tự động mở trình duyệt khi chạy ứng dụng
+    host: '127.0.0.1',  // Hoặc '0.0.0.0' để truy cập từ mạng nội bộ
+    port: 5173,         // Cổng bạn đang sử dụng, có thể thay đổi nếu cần
+    open: true,        
   },
-})
+  build: {
+    outDir: 'dist', 
+    emptyOutDir: true,   
+    rollupOptions: {     
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], 
+        },
+      },
+    },
+  },
+});
