a7469c69b2fbb2424013a862c0cf041302f53ae8
PSThai <93112500+PSThai@users.noreply.github.com>
2024-12-04 00:48:42 +0700

    "update createBooking"

--- backend/src/controllers/bookingController.js
+++ backend/src/controllers/bookingController.js
@@ -1,10 +1,50 @@
 const Booking = require('../models/Booking');
 const Trips = require('../models/Trips');
+const Bull = require('bull');
+const Redis = require('ioredis');
+const redis = new Redis();
+require('dotenv').config();
+const paymentQueue = new Bull('paymentQueue', {
+  // redis: { host: 'localhost', port: 6379 } 
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    //password: process.env.REDIS_PASSWORD
+  }
+
+});
+redis.ping().then(() => {
+  console.log('Connected to Redis successfully!');
+}).catch(err => {
+  console.error('Error connecting to Redis:', err);
+});
+// console.log("process.env.REDIS_HOST",process.env.REDIS_HOST);
+//   console.log(" process.env.REDIS_PORT",process.env.REDIS_PORT);
+paymentQueue.process(async (job) => {
+  const { bookingId, tripId, seatId } = job.data;
+  const booking = await Booking.findById(bookingId);
+  if (booking.paymentStatus === 'Đang chờ thanh toán') {
+    booking.paymentStatus = 'Thanh toán không thành công';
+    console.log("Updated paymentStatus:", booking.paymentStatus);
+    await booking.save();
+    const trip = await Trips.findById(tripId);
+    trip.tripDates.forEach(date => {
+      const initialBookedSeatsLength = date.bookedSeats.booked.length;
+      date.bookedSeats.booked = date.bookedSeats.booked.filter(seat => seat.seatId !== seatId);
+      if (date.bookedSeats.booked.length < initialBookedSeatsLength) {
+        console.log(`Ghế với seatId: ${seatId} đã được xóa thành công.`);
+      }
+    });
+    await trip.save();
+  }
+});
+
+
 
 const createBooking = async (req, res) => {
-  
+
   const { tripId, userId, seatId, totalFare, selectedDepartureName, selectedDestinationName, Timehouse, departureDate, passengerInfo } = req.body;
-  
+
   if (!tripId || !userId || !seatId || !totalFare || !passengerInfo) {
     return res.status(400).send('Thiếu dữ liệu yêu cầu.');
   }
@@ -12,7 +52,7 @@ const createBooking = async (req, res) => {
   const lastBooking = await Booking.findOne().sort({ BookingID: -1 });
   const newBookingID = lastBooking ? lastBooking.BookingID + 1 : 1;
   const trip = await Trips.findById(tripId);
-  
+
   if (trip.bookedSeats && trip.bookedSeats.some(seat => seat.seatId === seatId)) {
     return res.status(400).send('Ghế đã được đặt');
   }
@@ -42,7 +82,7 @@ const createBooking = async (req, res) => {
     tripId,
     seatId
   }, {
-    delay: 5 * 60 * 1000 
+    delay: 5 * 60 * 1000
   });
   res.status(201).send(booking);
 };
@@ -137,7 +177,7 @@ const getBookingByUser = async (req, res) => {
     res.status(500).send('Đã xảy ra lỗi khi truy vấn dữ liệu.');
   }
 
-}  
+}
 const getBookingByUserId = async (req, res) => {
   const { userId } = req.query;
 
@@ -146,17 +186,21 @@ const getBookingByUserId = async (req, res) => {
   }
 
   try {
-    const bookings = await Booking.find({ userId }) .populate('tripId') 
-    .populate({path: 'tripId', populate: {path: 'userId',select: 'fullName phoneNumber',}
-      ,})
-    .populate({path: 'tripId', populate: {path: 'routeId',select: 'departure destination',}
-        ,})
-    .exec();
-    res.status(200).json(bookings);  
+    const bookings = await Booking.find({ userId }).populate('tripId')
+      .populate({
+        path: 'tripId', populate: { path: 'userId', select: 'fullName phoneNumber', }
+        ,
+      })
+      .populate({
+        path: 'tripId', populate: { path: 'routeId', select: 'departure destination', }
+        ,
+      })
+      .exec();
+    res.status(200).json(bookings);
   } catch (error) {
     console.error(error);
     res.status(500).send('Đã xảy ra lỗi khi truy vấn dữ liệu.');
   }
 }
 
-module.exports = { createBooking,getBookingByUser, getBookingByUserId };
+module.exports = { createBooking, getBookingByUser, getBookingByUserId };
